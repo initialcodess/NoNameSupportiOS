2cd58d76ae1379d2db16ca7a00afce8092eb5ec7
Initial Code <engineering@initialcode.com.tr>
2023-02-27 02:17:23 +0300

    first commit

---
+++ NoNameSupportiOS/NoNameSupportiOS/NoNameSupportiOS.h
@@ -0,0 +1,18 @@
+//
+//  NoNameSupportiOS.h
+//  NoNameSupportiOS
+//
+//  Created by Burak Soykal on 27.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NoNameSupportiOS.
+FOUNDATION_EXPORT double NoNameSupportiOSVersionNumber;
+
+//! Project version string for NoNameSupportiOS.
+FOUNDATION_EXPORT const unsigned char NoNameSupportiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoNameSupportiOS/PublicHeader.h>
+
+
